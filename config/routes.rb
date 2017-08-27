32beda51cb619d52fa2bd9a8754a96d2521a836d
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-08-27 12:02:27 -0300

    Setting devise and posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :user
+
+  devise_scope :user do
+    get 'admin/login', to: 'devise/sessions#new'
+  end
+
   root to: "home#index"
+
+  namespace :admin do
+    resources :posts
+  end
 end
