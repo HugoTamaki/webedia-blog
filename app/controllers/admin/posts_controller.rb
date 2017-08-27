450e50f9ba2410a98ce3ae03e7ac97a787a8eeaf
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-08-27 12:03:07 -0300

    Creating posts controller

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,29 @@
+module Admin
+  class PostsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+      @posts = Post.all
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.new(post_params)
+
+      if @post.save
+        redirect_to admin_posts_path, notice: "Post criado com sucesso!"
+      else
+        render :new, alert: "Verifique os campos e tente novamente."
+      end
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :subtitle, :content)
+    end
+  end
+end
